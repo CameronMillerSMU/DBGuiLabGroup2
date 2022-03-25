9719357f799f983fbeffc683f5235d0105894df7
blake-gebhardt <blakewgebhardt@gmail.com>
2022-03-24 20:02:30 -0500

    making the database, plants, locations, and users.

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,18 +1,44 @@
 -- create database db
-CREATE DATABASE db;
+CREATE DATABASE flora;
 
 -- use newly create database
-USE db;
+USE flora;
 
 -- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+CREATE TABLE flora.plants (
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(100),
+    category VARCHAR(30),
+    climate VARCHAR(30),
+    imagePath VARCHAR(200),
+    PRIMARY KEY (name)
 );
 
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+SELECT * FROM plants;
+
+CREATE TABLE flora.location(
+    cityName VARCHAR(30),
+    tempLow int,
+    tempHigh int,
+    lastUpdate DATETIME,
+    weatherType VARCHAR(30),
+    nearestStore VARCHAR(30),
+    PRIMARY KEY (cityName)
+);
+
+SELECT * FROM location;
+
+CREATE TABLE flora.user(
+    username VARCHAR(30) NOT NULL,
+    passwordHash VARCHAR(30) NOT NULL,
+    birthday DATE NOT NULL,
+    location VARCHAR(30),
+    adminTag BOOLEAN NOT NULL,
+    registerTag BOOLEAN NOT NULL,
+    privateTag BOOLEAN NOT NULL,
+    backgroundPath VARCHAR(200),
+    PRIMARY KEY (username));
+
+
 
 
