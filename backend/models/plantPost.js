ea7adb196690344cd9fb57ddb0b77819716dbd7e
BurnsClark-McKay <bclarkmckay@mail.smu.edu>
2022-04-08 09:33:23 -0500

    Made plantPost.js

---
+++ backend/models/plantPost.js
@@ -0,0 +1,72 @@
+const knex = require('knex');
+
+const POST_TABLE = 'plantPost';
+
+const createNewPlantPost = async (topic, poster, title, post, privateTag) => {
+
+    const query = knex(POST_TABLE).insert({ topic, poster, title, post, privateTag, likeCounter: 0});
+    console.log('Raw query for createNewPlantPost:', query.toString());
+    const result = await query;
+
+    return result;
+};
+
+const getAllPlantPosts = async () => {
+    const query = knex(POST_TABLE);
+    const result = await query;
+    return result;
+}
+
+const getAllPlantPostsExcludePrivate = async () => {
+    const query = knex(POST_TABLE).where({privateTag: 0});
+    const result = await query;
+    return result;
+}
+
+const findPlantPostByPostId = async (postId, ) => {
+    const query = knex(POST_TABLE).where({ postId});
+    const result = await query;
+    return result;
+}
+
+const findPlantPostByPostIdExcludePrivate = async (postId, ) => {
+    const query = knex(POST_TABLE).where({ postId, privateTag: 0 });
+    const result = await query;
+    return result;
+}
+
+const findPlantPostsByPoster = async (poster) => {
+    const query = knex(POST_TABLE).where({ poster});
+    const result = await query;
+    return result;
+}
+
+const findPlantPostsByPosterExcludePrivate = async (poster) => {
+    const query = knex(POST_TABLE).where({ poster, privateTag: 0 });
+    const result = await query;
+    return result;
+}
+
+const findPlantPostsByTopic = async (topic) => {
+    const query = knex(POST_TABLE).where({ topic});
+    const result = await query;
+    return result;
+}
+
+const findPlantPostsByTopicExcludePrivate = async (topic) => {
+    const query = knex(POST_TABLE).where({ topic, privateTag: 0 });
+    const result = await query;
+    return result;
+}
+
+module.exports = {
+    createNewPlantPost,
+    findPlantPostByPostId,
+    findPlantPostsByPoster,
+    findPlantPostsByTopic,
+    findPlantPostByPostIdExcludePrivate,
+    findPlantPostsByPosterExcludePrivate,
+    findPlantPostsByTopicExcludePrivate,
+    getAllPlantPosts,
+    getAllPlantPostsExcludePrivate
+};
