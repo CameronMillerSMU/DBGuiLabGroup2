f6f642beb131639c52eb43acda63f8f358db60a0
BurnsClark-McKay <bclarkmckay@mail.smu.edu>
2022-04-14 19:41:45 -0500

    Added Update and Delete functionality to forum.js and like.js models

--- backend/models/forum.js
+++ backend/models/forum.js
@@ -15,30 +15,44 @@ const getAllForums = async () => {
     const query = knex(FORUM_TABLE);
     const result = await query;
     return result;
-}
+};
 
 const getAllForumsExcludePrivate = async () => {
     const query = knex(FORUM_TABLE).where({ privateTag: 0 });
     const result = await query;
     return result;
-}
+};
 
 const findForumByTopic = async (topic) => {
     const query = knex(FORUM_TABLE).where({ topic });
     const result = await query;
     return result;
-}
+};
 
 const findForumByTopicExcludePrivate = async (topic) => {
     const query = knex(FORUM_TABLE).where({ topic, privateTag: 0 });
     const result = await query;
     return result;
-}
+};
+
+const updateForumDescription = async (topic, new_description) => {
+    const query = knex(FORUM_TABLE).where({topic}).update({description: new_description});
+    const result = await query;
+    return result;
+};
+
+const deleteForum = async (topic) => {
+    const query = knex(FORUM_TABLE).where({ topic }).del();
+    const result = await query;
+    return result;
+};
 
 module.exports = {
     createNewForum,
     findForumByTopic,
     getAllForums,
     findForumByTopicExcludePrivate,
-    getAllForumsExcludePrivate
+    getAllForumsExcludePrivate,
+    updateForumDescription,
+    deleteForum
 };
