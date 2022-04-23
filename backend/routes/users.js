57ef03e5b99cba3a8f91b0f23815c09755ed16c7
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-04-23 18:10:54 -0500

    Update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,12 +19,10 @@ module.exports = function routes(app, logger) {
   app.post('/register', async (req, res) => {
     try {
       const body = req.body;
-      console.log(body[0]);
-      console.log(body[1]);
       result = await User.createNewUser(body.username, body.password);
       if (result.success) {
-        const token = await UserController.authenticateUser(body.username, body.password);
-        return res.status(201).json(token); } 
+        result = await User.findByUserName(body.username);
+        return res.status(201).json(result[0]); } 
       else { return res.status(400).json(result); }
     } catch (err) {
       return res.status(400).json({ message: 'Duplicate Entry' });
@@ -38,9 +36,9 @@ module.exports = function routes(app, logger) {
       const result = await UserController.authenticateUser(body.username, body.password);
       if (result == null) {
         return res.status(401).json({ message: 'Body Does Not Match Existing Credentials' }); }
-      return res.status(200).send({ data: { jwt: result, username }});
+      return res.status(201).json(result);
     } catch (err) {
-      return res.status(400).json({ message: 'Body Does Not Match Existing Credentials' });
+      return res.status(401).json({ message: 'Body Does Not Match Existing Credentials' });
     }
   });
 
@@ -157,18 +155,6 @@ module.exports = function routes(app, logger) {
       return res.status(200).json(result);
     } catch (err) {
       return res.status(401).json({ message: 'Could Not Update Privacy' });
-    }
-  });
-
-  // Update Admin (0 = False, 1 = True, TINYINT)
-  app.put('/updateadmin', authenticateJWT, async (req, res) => {
-    try {
-      const body = req.body;
-      result = await User.updateAdmin(body.username, body.admin);
-      result = await User.findByUserName(body.username);
-      return res.status(200).json(result);
-    } catch (err) {
-      return res.status(401).json({ message: 'Could Not Update Admin' });
     }
   });
 
