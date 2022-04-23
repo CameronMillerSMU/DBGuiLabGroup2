bda805e7ef9227d042665b940993c3c58b63e569
eridge11 <72117650+eridge11@users.noreply.github.com>
2022-04-23 16:45:23 -0500

    fixed

--- frontend/src/common/ApiCalls.js
+++ frontend/src/common/ApiCalls.js
@@ -2,15 +2,23 @@ import axios from 'axios';
 import { apiEndpoint, apiConfig } from './ApiConfig';
 
 
-
-export const addUser = (user) => newPromise((resolve, reject) => {
+export const addUser = (user) => new Promise((resolve, reject) => {
     user[0] = "thisismyusername";
     user[1] = "thisismypassword";
     
-    axois.post(`${apiEndpoint}/users/${user[0]}/${user[1]}`, apiConfig)
+    axios.post(`${apiEndpoint}/users/${user[0]}/${user[1]}`, apiConfig)
     .then(x => resolve(x.data))
     .catch(x => {
         alert(x);
         reject(x);
     });
+});
+
+export const getUserById = (id) = new Promise((resolve, reject) => {
+    axios.get(`${apiEndpoint}/users/${id}`, apiConfig)
+        .then(x => resolve(x.data))
+        .catch(x => {
+            alert(x);
+            reject(x);
+        });
 });
