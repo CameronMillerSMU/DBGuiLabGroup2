97c7085eeea26437bc3773390c22c35773565097
BurnsClark-McKay <bclarkmckay@mail.smu.edu>
2022-04-24 17:13:08 -0500

    Location: Models -> Controllers?

--- backend/controllers/location.js
+++ backend/controllers/location.js
@@ -5,32 +5,61 @@ const Location = require('../models/location');
 const accessTokenSecret = 'mysupercoolsecret';
 
 const createNewLocation = async (cityName, tempLow, tempHigh, lastUpdate, weatherType, nearestStore) => {
-    const query = Location.createNewLocation(cityName, tempLow, tempHigh, lastUpdate, weatherType, nearestStore);
+    const query = location.createNewLocation(cityName, tempLow, tempHigh, lastUpdate, weatherType, nearestStore);
     console.log('Raw query for createNewLocation:', query.toString());
     const result = await query;
     return result;
 };
 
 const getAllLocations = async () => {
-    const query = Location.getAllLocations();
+    const query = location.getAllLocations();
     const result = await query;
     return result;
-}
+};
 
 const findLocationByName  = async (name) => {
-    const query = Location.findLocationByName(name);
+    const query = location.findLocationByName(name);
     const result = await query;
     return result;
-}
+};
 
-const findLocationByWeather  = async (weather) => {
-    const query = Location.findLocationByWeather(weather);
+const findLocationByWeather  = async (weatherType) => {
+    const query = location.findLocationByWeather(weatherType);
     const result = await query;
     return result;
-}
+};
+
+const updateLocationTemps = async (cityName, tempLow, tempHigh) => {
+    const query = location.updateLocationTemps(cityName, tempLow, tempHigh);
+    const result = await query;
+    return result;
+};
+
+const updateLocationWeather = async (cityName, weatherType) => {
+    const query = location.updateLocationWeather(cityName, weatherType);
+    const result = await query;
+    return result;
+};
+
+const updateLocationStore = async (cityName, nearestStore) => {
+    const query = location.updateLocationTemps(cityName, nearestStore);
+    const result = await query;
+    return result;
+};
+
+const deleteLocation = async (cityName) => {
+    const query = loction.deleteLocation();
+    const result = await query;
+    return result;
+};
 
 module.exports = {
+    createNewLocation,
     getAllLocations,
     findLocationByName,
-    findLocationByWeather
+    findLocationByWeather,
+    updateLocationTemps,
+    updateLocationWeather,
+    updateLocationStore,
+    deleteLocation
 };
