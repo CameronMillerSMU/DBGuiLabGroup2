2c51dc43530b03d4f5355d701acf5267a5b67afd
BurnsClark-McKay <bclarkmckay@mail.smu.edu>
2022-04-24 20:43:48 -0500

    MVC for location completed I think, unnecessary steps included because I don't want to cut and then re-link them.

--- backend/routes/location.js
+++ backend/routes/location.js
@@ -19,7 +19,7 @@ module.exports = function routes(app, logger){
               return res.status(401).json({ message: "Failed to make a new location." });
             }
         
-    })
+    });
 
 
     app.get('/allLocations', authenticateJWT, async (req, res) => {
@@ -30,7 +30,7 @@ module.exports = function routes(app, logger){
           } catch (err) {
             res.status(401).json({ message: 'Could Not Get All Locations'});
           }
-        })
+        });
 
 
     app.get('/locationByCityName', authenticateJWT, async (req, res, next) => {
@@ -48,7 +48,67 @@ module.exports = function routes(app, logger){
             res.sendStatus(500).json({ message: err.toString() });
         }
 
-})
+});
+
+app.get('/locationByWeather', authenticateJWT, async (req, res, next) => {
+    try {
+        var stadio = req.query.owner;
+        if (req.query.owner == undefined){
+            stadio = null;
+        }
+        const result = await locationController.findLocationByWeather(stadio);
+        console.log("HERE RESULT: " + result);
+        res.status(200).json(result);
+        
+    } catch (err) {
+        console.error("Failed to get this location: ", err);
+        res.sendStatus(500).json({ message: err.toString() });
+    }
+
+});
+
+app.put('/updateLocationTemps', authenticateJWT, async (req, res) => {
+    try {
+      const body = req.body;
+      result = await locationController.updateLocationTemps(body.cityName, body.tempLow, body.tempHigh);
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(401).json({ message: 'Could Not Update temperatures' });
+    }
+  });
+
+  app.put('/updateLocationWeather', authenticateJWT, async (req, res) => {
+    try {
+      const body = req.body;
+      result = await locationController.updateLocationWeather(body.cityName, body.weatherType);
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(401).json({ message: 'Could Not Update weather' });
+    }
+  });
+
+  app.put('/updateLocationStore', authenticateJWT, async (req, res) => {
+    try {
+      const body = req.body;
+      result = await locationController.updateLocationStore(body.cityName, body.nearestStore);
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(401).json({ message: 'Could Not Update store' });
+    }
+  });
+
+  app.delete('/deleteLocation', authenticateJWT, async (req, res) => {
+    try {
+      const body = req.body;
+      result = await User.findLocationByName(body.cityName);
+      if (Object.keys(result).length == 0) {
+        return res.status(401).json({ message: 'Location Does Not Exist' }); }
+      result = await locationController.deleteLocation(body.cityName);
+      return res.status(204).json(result);
+    } catch (err) {
+      return res.status(401).json({ message: 'Could Not Delete location' });
+    }
+  });
 
 
 
