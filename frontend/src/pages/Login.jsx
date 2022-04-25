48c4b195efce879552c2524d1804200440af35cb
CameronMillerSMU <78335921+CameronMillerSMU@users.noreply.github.com>
2022-04-25 15:18:32 -0500

    making sign up styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -34,19 +34,19 @@ export const Login = () => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
     ApiCall.login(data.get('username'), data.get('password')).then(result => {
-        if(result.status <= 201) {
-          ApiCall.getToken().then(response => {
-            console.log('Result: ');
-            console.log(response);
-            sessionStorage.setItem('username', response.data.username);
-            sessionStorage.setItem('password', response.data.password);
-          })
+      if (result.status <= 201) {
+        ApiCall.getToken().then(response => {
+          console.log('Result: ');
+          console.log(response);
+          sessionStorage.setItem('username', response.data.username);
+          sessionStorage.setItem('password', response.data.password);
+        })
           .catch(error1 => {
             console.log('Error: ')
             console.log(error1);
           });
-          navigate('/home');
-        }
+        navigate('/home');
+      }
 
     }).catch(error2 => {
       alert("Username or Password incorrect");
@@ -54,28 +54,87 @@ export const Login = () => {
   };
 
   return <>
-  <Box component="form" noValidate onSubmit={handleSignUp}>
-    <TextField
-      name = "username"
-      required
-      id="username"
-      label="username"
-    ></TextField>
-    <TextField
-      name = "password"
-      required
-      id="password"
-      label="password"
-    ></TextField>
-    <Button
-      type = "submit"
-      variant = "outlined"
-    >Submit</Button>
-    <Button
-        onClick={() => navigate("/register")}
-        variant = "outlined"
-      >
-      Don't have an account? Sign Up!</Button>
-  </Box>
+    <div className="w-75 mx-auto">
+      <div className="border mb-2 mt-5">
+        <h1 className="text-white bg-primary p-3 mb-0">Login</h1>
+        <Box component="form" noValidate onSubmit={handleSignUp} className="bg-white py-2 mt-0">
+          <div className="mb-5 ms-3 col-md-4" controlId="username">
+            <TextField 
+              placeholder="Enter Your Username"
+              name="username"
+              required
+              id="username"
+              label="username" >
+            </TextField>
+          </div>
+          <div className="mb-2 ms-3 col-md-4" controlId="password">
+            <TextField 
+              placeholder="Enter Your Password"
+              name="password"
+              required
+              id="password"
+              label="password"
+            ></TextField>
+          </div>
+          <Button
+            type="submit"
+            variant="outlined"
+          >Submit</Button>
+          <p className="ms-3">Need an account? <Link to="/register">Sign Up</Link></p>
+        </Box>
+      </div>
+      <Link to="/home" className="btn btn-danger me-3">Cancel</Link>
+      <button className="btn btn-success" id="loginButton" type="submit" form="login-form">Submit</button>
+    </div>
   </>;
 }
+
+/*
+<div className="w-75 mx-auto">
+            <div className="border mb-2 mt-5">
+                <h1 className="text-white bg-primary p-3 mb-0">Login</h1>
+                <Form noValidate validated={validated} id="login-form" onSubmit={handleLogin} className="bg-white py-2 mt-0">
+                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="username">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Your Username" value={username} onChange={(event) => setUsername(event.target.value)} required />
+                        <Form.Control.Feedback type="invalid"> Please enter a username.</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Your Password" value={password} onChange={(event) => setPassword(event.target.value)} required />
+                        <Form.Control.Feedback type="invalid"> Please enter a password.</Form.Control.Feedback>
+                    </Form.Group>
+                    <p className="ms-3">Need an account? <Link to="/Signup">Sign Up</Link></p>
+                </Form>
+                
+            </div>
+            <Link to="/" className="btn btn-danger me-3">Cancel</Link>
+            <button className="btn btn-success" id="loginButton" type="submit" form="login-form">Submit</button>
+            
+        </div>
+        */
+
+        /*
+<div className="w-75 mx-auto">
+            <div className="border mb-2 mt-5">
+                <h1 className="text-white bg-primary p-3 mb-0">Login</h1>
+                <Form noValidate validated={validated} id="login-form" onSubmit={handleLogin} className="bg-white py-2 mt-0">
+                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="username">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Your Username" value={username} onChange={(event) => setUsername(event.target.value)} required />
+                        <Form.Control.Feedback type="invalid"> Please enter a username.</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Your Password" value={password} onChange={(event) => setPassword(event.target.value)} required />
+                        <Form.Control.Feedback type="invalid"> Please enter a password.</Form.Control.Feedback>
+                    </Form.Group>
+                    <p className="ms-3">Need an account? <Link to="/Signup">Sign Up</Link></p>
+                </Form>
+                
+            </div>
+            <Link to="/" className="btn btn-danger me-3">Cancel</Link>
+            <button className="btn btn-success" id="loginButton" type="submit" form="login-form">Submit</button>
+            
+        </div>
+        */
