8479dd71ec374eb39b8c977839a73bd12c57b22d
eridge11 <72117650+eridge11@users.noreply.github.com>
2022-04-25 20:07:45 -0500

    Problem: can't get buttons to work

--- frontend/src/common/Banner.jsx
+++ frontend/src/common/Banner.jsx
@@ -12,11 +12,13 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import YardIcon from '@mui/icons-material/Yard';
+import { Navigate, useNavigate } from 'react-router-dom';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Home', 'Plants', 'Login'];
+const settings = ['Profile', 'Logout'];
 
 export const Banner = () => {
+  const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -28,7 +30,7 @@ export const Banner = () => {
   };
 
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+    setAnchorElNav(Navigate('/'));
   };
 
   const handleCloseUserMenu = () => {
@@ -45,10 +47,13 @@ export const Banner = () => {
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-             Flora&nbsp;  
+            Flora&nbsp;
             <YardIcon fontSize='large' margin-left='25%' />
           </Typography>
 
+
+
+          
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -78,20 +83,39 @@ export const Banner = () => {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
+              
+            <MenuItem>
+              <Typography textAlign="center"></Typography>
+                <Button
+                  href="/register"
+                >
+                  Home!
+                </Button>
+            </MenuItem>
+            
             </Menu>
           </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
-            LOGO
+            Flora&nbsp;
+            <YardIcon fontSize='large' margin-left='25%' />
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
@@ -127,15 +151,22 @@ export const Banner = () => {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              
             </Menu>
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
+
+
+
+
+
+
+
+
+
+
+
 };
